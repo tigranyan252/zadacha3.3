60b413d8e38c31a6ba0c940aeb72e9c2c0011d6e
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-11-23 21:58:22 +0200

    Добавьте файлы проекта.

---
+++ zadacha3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zadacha3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите i:");
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите t:");
+                double t = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите s:");
+                double s = int.Parse(Console.ReadLine());
+                for (int n > 0; n = ; i++)
+
+
+
+
+
+            }
+        }
+    }
+}
